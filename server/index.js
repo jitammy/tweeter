d04ad70b1afebffe99fa9a9fbb4250de46f96723
jitammy <jitammy@gmail.com>
2019-05-16 18:55:37 +0000

    Switching Tweeter to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
 "use strict";
-
 // Basic express setup:
-
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const morgan        = require('morgan');
-
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan('short'));
 
+MongoClient.connect(MONGODB_URI, (err, database) => {
+  var db = database.db('tweeter');
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+}
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
-
+// const db = require("./lib/in-memory-db");
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
@@ -26,13 +30,15 @@ const db = require("./lib/in-memory-db");
 // require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
 
+DataHelpers.getTweets(function(data) {
+  console.log(data);
+})
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
-
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+})
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,12 +1,9 @@
 "use strict";
-
 const userHelper    = require("../lib/util/user-helper")
-
 const express       = require('express');
 const tweetsRoutes  = express.Router();
-
 module.exports = function(DataHelpers) {
-
+  // get tweets
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -16,13 +13,12 @@ module.exports = function(DataHelpers) {
       }
     });
   });
-
+  // post tweets
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
-
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
     const tweet = {
       user: user,
@@ -31,7 +27,6 @@ module.exports = function(DataHelpers) {
       },
       created_at: Date.now()
     };
-
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -40,7 +35,5 @@ module.exports = function(DataHelpers) {
       }
     });
   });
-
   return tweetsRoutes;
-
 }
